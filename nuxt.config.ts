61fd6108036aa2e7c5272c507ad5c488dcffcc04
trankhacbinhduong <trankhacbinhduong@gmail.com>
2024-11-06 21:50:39 +0700

    :thumbsup: Add @nuxt/ui module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
 export default defineNuxtConfig({
+  extends: ['@nuxt/ui-pro'],
   modules: [
     '@nuxthub/core',
     '@nuxt/eslint',
     'nuxt-auth-utils',
+    '@nuxt/ui',
   ],
   devtools: { enabled: true },
   future: { compatibilityVersion: 4 },
